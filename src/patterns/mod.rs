44455374c193793c3882dfb02e8dce3b4798c689
YogirajT <tawdeyogiraj@yahoo.com>
2023-03-15 02:38:31 +0530

    singleton added

--- src/patterns/mod.rs
+++ src/patterns/mod.rs
@@ -1,5 +1,6 @@
 pub mod builder;
 pub mod factory;
 pub mod license;
+pub mod singleton;
 pub mod state;
 pub mod state_encoding_into_type_system;
---
+++ src/patterns/singleton.rs
@@ -0,0 +1,56 @@
+#![allow(dead_code)]
+
+use std::sync::{Arc, Mutex};
+
+struct Counter {
+    count: i32,
+}
+
+impl Counter {
+    fn new() -> Self {
+        Counter { count: 0 }
+    }
+
+    fn add(&mut self) {
+        self.count += 1;
+    }
+
+    fn get_count(&self) -> i32 {
+        self.count
+    }
+}
+
+lazy_static::lazy_static! {
+    static ref COUNTER_SINGLETON: Arc<Mutex<Counter>> = Arc::new(Mutex::new(Counter::new()));
+}
+
+#[cfg(test)]
+mod builder_tests {
+    use crate::patterns::singleton::COUNTER_SINGLETON;
+
+    #[test]
+    fn test_singleton() {
+        let available_threads = 4;
+
+        let mut threads = Vec::new();
+
+        for _ in 0..available_threads {
+            let counter_clone = COUNTER_SINGLETON.clone();
+
+            threads.push(std::thread::spawn(move || {
+                let mut counter = counter_clone.lock().unwrap();
+                counter.add();
+            }))
+        }
+
+        for th in threads {
+            th.join().unwrap();
+        }
+
+        let counter_clone = COUNTER_SINGLETON.clone();
+
+        let counter = counter_clone.lock().unwrap();
+
+        assert_eq!(available_threads, counter.get_count());
+    }
+}
