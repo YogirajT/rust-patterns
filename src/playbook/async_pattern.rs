06c1faf65b63f27af1ac9c888c539a832f11f3f5
YogirajT <tawdeyogiraj@yahoo.com>
2023-03-30 03:11:54 +0530

    readfile

--- src/playbook/async_pattern.rs
+++ src/playbook/async_pattern.rs
@@ -1,5 +1,32 @@
+use std::io;
+use std::str;
+
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+pub async fn read_file() -> io::Result<()> {
+    let mut f = File::open("foo.txt").await?;
+    let mut buffer = [0; 10];
+
+    // read up to 10 bytes
+    let n = f.read(&mut buffer[..]).await?;
+
+    println!("The bytes: {:?}", &buffer[..n]);
+
+    let s = match str::from_utf8(&buffer[..n]) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {e}"),
+    };
+
+    println!("The string: {:?}", &s);
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod async_tests {
+
+    use super::read_file;
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn async_test() {
@@ -19,6 +46,6 @@ mod async_tests {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
     async fn async_test3() {
-        assert!(true);
+        let _err = read_file().await;
     }
 }
