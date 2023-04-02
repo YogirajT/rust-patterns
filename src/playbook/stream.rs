dcf1eec7be9f38ffa319af8dc94c4717ea37b5db
YogirajT <tawdeyogiraj@yahoo.com>
2023-04-02 23:49:42 +0530

    mutable stream

--- src/playbook/stream.rs
+++ src/playbook/stream.rs
@@ -4,7 +4,22 @@ mod stream_tests {
 
     #[tokio::test]
     async fn basic_stream_test() {
-        let stream = stream::iter(vec![17, 19]);
-        assert_eq!(vec![17, 19], stream.collect::<Vec<i32>>().await);
+        let stream = stream::iter(vec![1, 2]);
+        assert_eq!(vec![1, 2], stream.collect::<Vec<i32>>().await);
+    }
+
+    #[tokio::test]
+    async fn mut_curr_stream_test() {
+        let mut input = 2;
+        let mut pow2 = stream::repeat_with(|| {
+            let tmp = input;
+            input *= 2;
+            tmp
+        });
+
+        assert_eq!(Some(2), pow2.next().await);
+        assert_eq!(Some(4), pow2.next().await);
+        assert_eq!(Some(8), pow2.next().await);
+        assert_eq!(Some(16), pow2.next().await);
     }
 }
