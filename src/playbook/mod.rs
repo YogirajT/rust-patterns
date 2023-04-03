40fdb9fc35f31ba9df68acd8f5315076b4de22fc
YogirajT <tawdeyogiraj@yahoo.com>
2023-04-03 23:53:46 +0530

    smart pointers test

--- src/playbook/mod.rs
+++ src/playbook/mod.rs
@@ -1,6 +1,7 @@
 pub mod async_pattern;
 pub mod borrow_checker_quirks;
 pub mod raw_pointers;
+pub mod smart_pointers;
 pub mod stream;
 pub mod strtok;
 pub mod unsafe_ways;
---
+++ src/playbook/smart_pointers.rs
@@ -0,0 +1,33 @@
+pub trait Messenger {
+    fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T>
+where
+    T: Messenger,
+{
+    pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+        LimitTracker {
+            messenger,
+            value: 0,
+            max,
+        }
+    }
+
+    pub fn set_value(&mut self, value: usize) {
+        self.value = value;
+    }
+}
+
+#[cfg(test)]
+mod raw_pointer_tests {
+
+    #[test]
+    fn test_pointer_1() {}
+}
