89e16f3253421bb91d927086bfb2bd0507cd968d
YogirajT <tawdeyogiraj@yahoo.com>
2023-04-07 19:13:44 +0530

    Lifetime hacks with strtok done

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use rust_patterns::patterns::state::get_driving_license;
+use std::str;
 
 fn main() -> Result<(), ()> {
     get_driving_license();
+
+    let raw_bytestring = br"\u{211D} is not escaped here";
+    println!("{:?}", raw_bytestring);
+
+    // Byte strings can have byte escapes...
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    // ...but no unicode escapes
+    // let escaped = b"\u{211D} is not allowed";
+    println!("Some escaped bytes: {:?}", escaped);
+
+    if let Ok(my_str) = str::from_utf8(escaped) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
     Ok(())
 }
--- src/playbook/strtok.rs
+++ src/playbook/strtok.rs
@@ -1,11 +1,17 @@
 #![allow(dead_code)]
 
 // Shedding light on variance and subtyping
-pub fn strtok<'a>(input: &'a mut &'a str, delimiter: char) -> &'a str {
+
+// Solution is to introduce another lifetime 'b so that the compiler can choose separate life
+// strtok is now generic over 2 lifetimes 1. mutable borrow and 2. the string that is being pointed to and we're returning reference into the string being pointed to
+/*
+   strtok returns the sufiix string after first instance of delimiter by moving pointer
+*/
+pub fn strtok<'a, 'b>(input: &'a mut &'b str, delimiter: char) -> &'b str {
     if let Some(i) = input.find(delimiter) {
         let prefix = &input[..i];
         let suffix = &input[(i + delimiter.len_utf8())..];
-        *input = suffix;
+        *input = suffix; //input points to the suffix now. in place replacement
         prefix
     } else {
         let prefix = *input;
@@ -18,15 +24,34 @@ pub fn strtok<'a>(input: &'a mut &'a str, delimiter: char) -> &'a str {
 mod strtok_tests {
     use super::strtok;
 
+    fn check_is_static(_: &'static str) {}
+
     #[test]
     fn test_strtok() {
         let mut s = "foo bar";
 
-        let delimiter: char = ' ';
+        check_is_static(s); // this coarses the lifetime to static
 
-        let foo = strtok(&mut s, delimiter);
+        // compiler can now choose the lifetime separately than picking one and sticking to it
+        //
+        // &'a mut &'a str      -vs- &'a      mut &'b str
+        // &'static mut &'a str -vs- &'static mut &'b str
+        // ^ compile error      -vs- ^ no error
+        let foo = strtok(&mut s, ' ');
 
         assert_eq!("foo", foo);
         // assert_eq!("bar", s);
+    }
+
+    #[test]
+    fn test_strtok_2() {
+        let mut s = "foo bar";
+
+        {
+            let foo = strtok(&mut s, ' ');
+            assert_eq!("foo", foo);
+        }
+
+        assert_eq!(s, "bar");
     }
 }
