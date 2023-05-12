9aa2456f3f0719ea66fcdcd1dc7354ea25e8b30b
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-05-12 23:20:14 +0000

    Test for template method pattern

--- src/patterns/behavioural/template_method.rs
+++ src/patterns/behavioural/template_method.rs
@@ -1,52 +1,72 @@
 #![allow(dead_code)]
+
 trait WomboCombo {
-    fn go(&self) {
+    fn go(&mut self) {
         self.move1();
         self.move2();
         self.move3();
     }
 
-    fn move1(&self);
-    fn move2(&self);
-    fn move3(&self);
+    fn move1(&mut self);
+    fn move2(&mut self);
+    fn move3(&mut self);
 }
 
-struct FalcoCombo;
+struct FalcoCombo {
+    out: String
+}
 
 impl WomboCombo for FalcoCombo {
-    fn move1(&self) {
-        println!("Falcon Punch");
+
+
+    fn move1(&mut self) {
+        self.out.push_str("Falcon Punch ");
     }
 
-    fn move2(&self) {
-        println!("Reverse Swing Kick");
+    fn move2(&mut self) {
+        self.out.push_str("Reverse Swing Kick ");
     }
 
-    fn move3(&self) {
-        println!("K/O");
+    fn move3(&mut self) {
+        self.out.push_str("K/O");
     }
 }
 
-struct FoxCombo;
+struct FoxCombo {
+    out: String
+}
 
 impl WomboCombo for FoxCombo {
-    fn move1(&self) {
-        println!("High Kick");
+    fn move1(&mut self) {
+        self.out.push_str("High Kick ");
     }
 
-    fn move2(&self) {
-        println!("Left Hook");
+    fn move2(&mut self) {
+        self.out.push_str("Left Hook ");
     }
 
-    fn move3(&self) {
-        println!("K/O");
+    fn move3(&mut self) {
+        self.out.push_str("K/O");
     }
 }
 
-fn main() {
-    let that_aint_falco = FalcoCombo;
-    let whoa = FoxCombo;
 
-    that_aint_falco.go();
-    whoa.go();
+#[cfg(test)]
+mod visitor_tests {
+    use super::*;
+
+    #[test]
+    fn test_template_method() {
+        let mut that_aint_falco = FalcoCombo{ out: String::new() };
+        let mut that_up_smash: FoxCombo = FoxCombo{ out: String::new() };
+
+        that_aint_falco.go();
+        that_up_smash.go();
+
+        let expected_output1 = "Falcon Punch Reverse Swing Kick K/O";
+        let expected_output2 = "High Kick Left Hook K/O";
+        assert_eq!(that_aint_falco.out, expected_output1);
+        assert_eq!(that_up_smash.out, expected_output2);
+    }
+
 }
