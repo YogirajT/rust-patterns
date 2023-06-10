2e4aa4a3cf242151d738c64f9ab589e848e46cce
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-06-10 22:46:03 +0000

    Fixed borrow errors

--- src/algorithms/graph/morris_traversal.rs
+++ src/algorithms/graph/morris_traversal.rs
@@ -2,7 +2,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 #[derive(Debug)]
-struct Node<T> {
+pub struct Node<T> {
     value: T,
     left: Option<Rc<RefCell<Node<T>>>>,
     right: Option<Rc<RefCell<Node<T>>>>,
@@ -20,7 +20,7 @@ impl<T> Node<T> {
 
 pub fn morris_traversal<T>(root: Option<Rc<RefCell<Node<T>>>>)
 where
-    T: std::fmt::Debug,
+    T: std::fmt::Debug + std::cmp::PartialEq,
 {
     let mut current = root;
 
@@ -30,10 +30,10 @@ where
             current = node.borrow().right.clone();
         } else {
             let mut predecessor = node.borrow().left.clone().unwrap();
-            while predecessor.borrow().right.is_some()
-                && predecessor.borrow().right.as_ref() != Some(&node)
+            while predecessor.clone().borrow().right.is_some()
+                && predecessor.as_ref().borrow().right.as_ref().unwrap().borrow().value != node.borrow().value
             {
-                predecessor = predecessor.borrow().right.clone().unwrap();
+                predecessor = predecessor.clone().borrow().right.clone().unwrap();
             }
 
             if predecessor.borrow().right.is_none() {
@@ -64,9 +64,9 @@ mod morris_tests {
         let node5 = Rc::new(RefCell::new(Node::new(5)));
 
         root.borrow_mut().left = Some(node2.clone());
-        root.borrow_mut().right = Some(node3.clone());
-        node2.borrow_mut().left = Some(node4.clone());
-        node2.borrow_mut().right = Some(node5.clone());
+        root.borrow_mut().right = Some(node3);
+        node2.borrow_mut().left = Some(node4);
+        node2.borrow_mut().right = Some(node5);
 
         // Perform Morris Traversal
         morris_traversal(Some(root));
