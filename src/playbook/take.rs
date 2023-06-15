3a88167294fd9d07bc454c5f2e730fbb70ea74c3
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-06-15 18:29:11 +0000

    WIP: take usecase

--- src/playbook/take.rs
+++ src/playbook/take.rs
@@ -1,12 +1,32 @@
-struct Buffer<T> {
+use std::mem::take;
+
+pub struct Buffer<T> {
     buf: Vec<T>,
 }
 
 impl<T> Buffer<T> {
     fn get_and_reset(&mut self) -> Vec<T> {
-        // error: cannot move out of dereference of `&mut`-pointer
         let buf = self.buf;
         self.buf = Vec::new();
         buf
+    }
+}
+
+impl<T> Buffer<T> {
+    fn get_and_reset_working(&mut self) -> Vec<T> {
+        take(&mut self.buf)
+    }
+}
+
+mod take_tests {
+    use super::*;
+
+    #[test]
+    fn take_test() {
+        let mut buffer = Buffer { buf: vec![1, 2, 3] };
+        assert_eq!(buffer.buf.len(), 3);
+
+        assert_eq!(buffer.get_and_reset_working(), vec![1, 2, 3]);
+        assert_eq!(buffer.buf.len(), 0);
     }
 }
