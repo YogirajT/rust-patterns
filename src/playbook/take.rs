20bdc39fa0fe8d9a875dc3622392371bdae775e5
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-06-16 23:30:58 +0000

    WIP take and default implementation

--- src/playbook/take.rs
+++ src/playbook/take.rs
@@ -1,19 +1,37 @@
-use std::mem::take;
+#![allow(dead_code)]
+use std::{mem::take};
 
-pub struct Buffer<T> {
-    buf: Vec<T>,
+pub struct Buffer {
+    buf: WrappedVec,
 }
 
-impl<T> Buffer<T> {
-    fn get_and_reset(&mut self) -> Vec<T> {
-        let buf = self.buf;
-        self.buf = Vec::new();
-        buf
+// this throws an error
+// impl<T> Buffer<T> {
+//     fn get_and_reset(&mut self) -> Vec<T> {
+//         let buf = self.buf;
+//         self.buf = Vec::new();
+//         buf
+//     }
+// }
+
+struct WrappedVec {
+    vec: Vec<i32>,
+}
+
+impl WrappedVec {
+    fn new() -> Self {
+        WrappedVec { vec: vec![1] }
     }
 }
 
-impl<T> Buffer<T> {
-    fn get_and_reset_working(&mut self) -> Vec<T> {
+impl Default for WrappedVec {
+    fn default() -> Self {
+        WrappedVec { vec: vec![1, 2] }
+    }
+}
+
+impl Buffer{
+    fn get_and_reset_working(&mut self) -> WrappedVec{
         take(&mut self.buf)
     }
 }
@@ -23,10 +41,14 @@ mod take_tests {
 
     #[test]
     fn take_test() {
-        let mut buffer = Buffer { buf: vec![1, 2, 3] };
-        assert_eq!(buffer.buf.len(), 3);
+        let mut buffer = Buffer {
+            buf: WrappedVec::new(),
+        };
+        assert_eq!(buffer.buf.vec.len(), 1);
 
-        assert_eq!(buffer.get_and_reset_working(), vec![1, 2, 3]);
-        assert_eq!(buffer.buf.len(), 0);
+        assert_eq!(buffer.get_and_reset_working().vec, vec![1]);
+
+        // takes value from default impl
+        assert_eq!(buffer.buf.vec.len(), 2);
     }
 }
