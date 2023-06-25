f97a4d376a0484db969e1a0d0257b291b42af473
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-06-25 18:29:30 +0000

    WIP floyd warshall

--- src/algorithms/graph/floyd_warshall.rs
+++ src/algorithms/graph/floyd_warshall.rs
@@ -1,16 +1,29 @@
-#[allow(dead_code)]
-
+#![allow(dead_code)]
+use std::{f32::{INFINITY}};
 
 // in-place algorithm to store in n^2 space
-pub fn generate_floyd_warshall_matrix(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+pub fn generate_floyd_warshall_matrix(input_adjecency_list: &Vec<&Vec<f32>>) -> Vec<Vec<f32>> {
 
-    let num_rows = input.len();
-    let num_cols = input.first().expect("first element has no length").len();
+    let num_rows = input_adjecency_list.len();
+    let num_cols = input_adjecency_list.first().expect("first element has no length").len();
 
 
     if num_rows != num_cols { panic!("invalid input: provide square matrix!")}
 
-    return vec![vec![]];
+    if num_rows == 0 { panic!("invalid input: provide square matrix!")}
+    if num_cols == 0 { panic!("invalid input: provide square matrix!")}
+
+    let mut fw_matrix: Vec<Vec<f32>> = vec![vec![INFINITY; num_cols]; num_rows];
+
+    for k in 0..num_cols {
+        for i in 0..num_cols {
+            for j in 0..num_rows {
+                fw_matrix[i][j] = f32::min(fw_matrix[i][j], fw_matrix[i][k] + fw_matrix[k][j]);
+            }
+        }
+    }
+
+    fw_matrix
 }
 
 
@@ -20,5 +33,9 @@ mod floyd_warshall_tests {
 
     #[test]
     fn test_floyd_warshall() {
+        let graph: Vec<Vec<f32>> = vec![ vec![ 0.0, 5.0, INFINITY, 10.0 ],
+                        vec![ INFINITY, 0.0, 3.0, INFINITY ],
+                        vec![ INFINITY, INFINITY, 0.0, 1.0 ],
+                        vec![ INFINITY, INFINITY, INFINITY, 0.0 ] ];
     }
 }
