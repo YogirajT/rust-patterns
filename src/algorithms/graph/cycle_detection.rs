c7c43ee340dda022550544b0c1512e459083a956
Yogi (Linux) <tawdeyogiraj@yahoo.com>
2023-07-13 18:27:17 +0000

    Fixed cyclic tests

--- src/algorithms/graph/cycle_detection.rs
+++ src/algorithms/graph/cycle_detection.rs
@@ -15,7 +15,7 @@ impl CycleDetection for Graph {
         let mut visited = vec![false; self.vertices];
 
         for v in 0..self.vertices {
-            if self.is_cyclic_util(v, &mut visited, None) {
+            if !visited[v] && self.is_cyclic_util(v, &mut visited, None) {
                 return true;
             }
         }
@@ -29,19 +29,16 @@ impl CycleDetection for Graph {
         visited: &mut Vec<bool>,
         parent: Option<usize>,
     ) -> bool {
-        if !visited[node_id] {
-            visited[node_id] = true;
+        visited[node_id] = true;
 
-            for &adj_vertex in &self.adj_list[node_id] {
-                if !visited[adj_vertex] {
-                    if self.is_cyclic_util(adj_vertex, visited, Some(node_id)) {
-                        return true;
-                    }
-                } else if parent.is_some_and(|val| !self.adj_list[node_id].contains(&val))  {
+        for &adj_vertex in &self.adj_list[node_id] {
+            if !visited[adj_vertex] {
+                if self.is_cyclic_util(adj_vertex, visited, Some(node_id)) {
                     return true;
                 }
+            } else if adj_vertex != parent.unwrap() {
+                return true;
             }
-
         }
 
         false
@@ -60,7 +57,6 @@ mod cyclic_check_1_tests {
         graph.add_edge(2, 0);
         graph.add_edge(2, 3);
 
-        println!("{:?}", graph);
         assert!(graph.has_cycle());
     }
 
@@ -69,8 +65,6 @@ mod cyclic_check_1_tests {
         let mut graph = Graph::new(4);
         graph.add_edge(0, 1);
         graph.add_edge(1, 2);
-
-        println!("{:?}", graph);
 
         assert!(!graph.has_cycle());
     }
